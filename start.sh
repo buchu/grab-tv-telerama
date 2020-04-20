90f115e543a69d4889bcdf8f29298b378ce7ae09
Romain Gaillegue <romain.gaillegue@orange.com>
2020-04-20 14:41:55 +0200

    Log to stdout

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 OUTPUT="/app"
 
 [ -f /app/.xmltv/tv_grab_fr_telerama.conf ] || echo -e "yes\nall" | tv_grab_fr_telerama --configure
-[ -f /app/all.xml ] || /usr/local/bin/tv_grab_fr_telerama --output /app/all.xml > /app/outpul.log 2>&1
+[ -f /app/all.xml ] || /usr/local/bin/tv_grab_fr_telerama --output /app/all.xml
 
 cd ${OUTPUT} && python3 -m http.server
 
